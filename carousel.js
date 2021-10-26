78c9e198d6b89f5a585b2bcc32d5f2092370dc51
Milad <miladxgh@outlook.com>
2021-10-26 21:31:45 +0330

    carousel works

--- carousel.js
+++ carousel.js
@@ -1,21 +1,49 @@
+// DOM elements
 const carousel = document.querySelector(".carousel");
 const prevBtn = document.querySelector(".btn-prev");
 const nextBtn = document.querySelector(".btn-next");
 const slides = document.querySelectorAll(".carousel img");
 
-let counter = 0;
+// variables
 const imageSize = slides[0].clientWidth;
+let direction = "";
 
+// next button event
 nextBtn.addEventListener("click", () => {
-  counter++;
-  carousel.style.transform = `translateX(${-imageSize * counter}px)`;
+  if (direction === "left") {
+    // to fix image order
+    carousel.prepend(carousel.lastElementChild);
+  }
+
+  direction = "right";
+  carousel.style.justifyContent = "flex-start";
+  carousel.style.transform = `translateX(${-imageSize}px)`;
 });
 
+// prev button event
 prevBtn.addEventListener("click", () => {
-  counter--;
-  carousel.style.transform = `translateX(${-imageSize * counter}px)`;
+  if (direction === "right") {
+    carousel.appendChild(carousel.firstElementChild);
+  }
+
+  direction = "left";
+  carousel.style.justifyContent = "flex-end";
+  carousel.style.transform = `translateX(${+imageSize}px)`;
 });
 
-// carousel.addEventListener("transitionend", () => {
-//   carousel.style.transform = "translateX(0)";
-// });
+carousel.addEventListener("transitionend", function () {
+  if (direction === "right") {
+    this.appendChild(this.firstElementChild);
+  } else {
+    this.prepend(this.lastElementChild);
+  }
+  // reset transition
+  this.style.transition = "none";
+  // reset because we dont want to see translaetX to 0
+  this.style.transform = "translateX(0)";
+  setTimeout(() => {
+    // set transition to "none" and after 1ms to "all 0.5s"
+    // if you dont use timeout(), transition "none" doesn't work!!!
+    this.style.transition = "all 0.5s";
+  }, 1);
+});
